25bd9f5a47c52cead00c03a6d566d30a03c79e9e
Joseph Obiajulu <obiajulu@princeton.edu>
2016-05-27 17:04:29 -0400

    created jl file off all the solvers in JuliaLang/ODE.jl master

---
+++ src/list_of_solvers.jl
@@ -0,0 +1,36 @@
+## Non-stiff fixed step solvers
+nonstiff_fixedstep= [
+           ODE.ode1,
+           ODE.ode2_midpoint,
+           ODE.ode2_heun,
+           ODE.ode4,
+           ODE.ode4ms,
+           ODE.ode5ms
+           ]
+
+## Non-stiff fixed step solvers
+nonstiff_adaptive=[
+           # adaptive
+#           ODE.ode21, # this fails on Travis with 0.4?! TODO revert once fixed.
+           ODE.ode23,
+           ODE.ode45_dp,
+           ODE.ode45_fe,
+           ODE.ode78
+           ]
+# Stiff fixed-step solvers
+stiff_fixedstep=[
+
+           ODE.ode4s_s,
+           ODE.ode4s_kr
+           ]
+#Stiff adaptive solvers
+stiff_adaptive = [
+           ODE.ode23s
+           ]
+
+#All of ODE.jl solvers by type
+all_solvers = [nonstiff_fixedstep,
+              nonstiff_adaptive,
+              stiff_fixedstep,
+              stiff_adaptive
+              ]
