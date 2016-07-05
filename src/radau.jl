1029f0921fb7c45ebe91d77dcb8c096c3e6ca87d
mayingbo <mayingbo5@gmail.com>
2016-07-05 14:29:11 -0400

    Add radau solver

---
+++ src/radau.jl
@@ -0,0 +1,28 @@
+#=
+    (1) done
+    (2) trialstep!()
+    (3) errorcontol!()
+    (4) odercontrol!()
+    (5) rollback!()
+    (6) status()
+=#
+
+function radau(f, y0, tspan)
+
+    #= setup
+        state.t, state.f(t)
+    =#
+
+    while !done()
+        stats = trialstep!(odep, st)
+        err, stats, st = errorcontrol!(odep, st) # (1) adaptive stepsize (2) error
+        if err < 1
+            stats, st = ordercontrol!()
+            accept = true
+        else
+            rollback!()
+        end
+
+        return = status()
+    end
+end
