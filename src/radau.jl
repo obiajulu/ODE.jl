05b0e332ba8054e68019fd93b1815b48e5485d87
Joseph Obiajulu <obiajulu@princeton.edu>
2016-07-05 17:18:55 -0400

    outline comments for trialstep

--- src/radau.jl
+++ src/radau.jl
@@ -42,6 +42,14 @@ function radau(f, y0, tspan, order ::Integer = 5)
     dypre = zeros(y0)
     step = 1
     finished = false
+
+    # Calculate c_i
+    c
+    # Calculate b_i
+    b
+    # Calculate a_ij
+    a
+    
     ## intialize state
     st =  RadauState{T,Y}(h,
                    t, y, dy,
@@ -74,4 +82,42 @@ end
 
 function trialstep!(st)
     @unpack st: h, t, tfinal
+    # Calculate simplified Jacobian
+    #     _                                             _
+    #    |M - h*a[11]*h*f(tn,yn) ... -h*a[1s]*f(tn,yn)   |
+    # J= |         ⋮              ⋱           ⋮           |
+    #    | - h*a[1s]*h*f(tn,yn) ...   M-h*a[ss]*f(tn,yn) |
+    #    |_                                             _|
+    #
+
+    # Use Netwon interation
+    #
+    #   ⃗k^(i+1) = ⃗k ^ (i) - J^{-1}*G(⃗k)
+    # where
+    #           /       \
+    #           |k_1^(i)|
+    #   ⃗k^(i) = |   ⋮   |
+    #           |k_s^(i)|
+    #           \       /
+    #
+    #   and
+    #
+    #           /            \
+    #          |f_1(k_1,…,k_s)|
+    #  G(k) =  |      ⋮       |  = 0
+    #          |f_s(k_1,…,k_s)|
+    #           \            /
+    #
+    #
+    #   and
+    #
+    #  f_i = k_i - f(t_n + c_ih, y_n + h*a_11*k1 + ⋯ + h*a_1s*k_s)
+    #
+    #
+
+    # Once Netwon method converges after some m steps, estimated next step size
+    #
+    #   y = ypre + h ∑b_i*k_i^{m}
+    #
+
 end
